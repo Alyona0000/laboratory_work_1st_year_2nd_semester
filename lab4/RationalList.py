1d889f4987934580d2f9d1d9bd4a48b8b9fc4165
LadyLyo <alena.rybakova61@gmail.com>
2025-05-12 22:18:02 +0300

    Remove old reader and argument classes; add new reader implementation and distribution logic for processing rational numbers from input files.

--- lab4/RationalList.py
+++ lab4/RationalList.py
@@ -47,18 +47,3 @@ class RationalList:
         for rational in self.rationals:
             total += rational
         return total
-
-
-def Distribution(file_name):
-    with open(file_name) as d: 
-        for line in d:
-
-            #print(f"Обробка списка: {line.strip()}")
-            elements = line.split()
-            rational_list = RationalList(elements)  
-
-            result = rational_list.sum()  
-            print(f"Сума чисел в списку: {result}")
-        print("---")
-
-Distribution("input03.txt")
