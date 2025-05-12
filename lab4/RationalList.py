ffbb4082b13bc94e7064e973a28aab53cb6a5452
LadyLyo <alena.rybakova61@gmail.com>
2025-05-12 10:34:31 +0300

    Enhance RationalList class; implement item access and modification via index, overload addition and in-place addition operators, and improve input handling for various types

--- lab4/RationalList.py
+++ lab4/RationalList.py
@@ -8,36 +8,84 @@ class RationalList:
     def __add__(self, other):
         if isinstance(other, RationalList):
             return RationalList(self.rationals + other.rationals)
+        elif isinstance(other, Rational):
+            return RationalList(self.rationals + [other])
+        elif isinstance(other, int):
+            return RationalList(self.rationals + [Rational(other, 1)])
         else:
-            return RationalList(self.rationals + [Rational(other)])
+            raise TypeError("Правий операнд має бути RationalList, Rational або int")
+        
+    def __getitem__(self, index): # Метод для отримання значення за індексом
+        return self.rationals[index]
 
-
-    def __getitem__(self, key): # Метод для отримання значення за ключем    
-        if key == "n":
-            return self.numerator
-        elif key == "d":
-            return self.denominator
+    def __setitem__(self, index, value): # Метод для встановлення значення за індексом
+        if isinstance(value, Rational):
+            self.rationals[index] = value
+        elif isinstance(value, int):
+            self.rationals[index] = Rational(value, 1)
+        elif isinstance(value, str):
+            self.rationals[index] = Rational(value)
         else:
-            raise KeyError("Ключ має бути 'n' або 'd'") 
-
-    def __setitem__(self, key, value): # Метод для встановлення значення за ключем
-        if key == "n":  
-            self.numerator = value
-        elif key == "d":  
-            self.denominator = value
-        else:
-            raise KeyError("Ключ має бути 'n' або 'd'") 
-
-
-    def __call__(self):
-        return self.numerator / self.denominator
+            raise TypeError("Можна встановлювати тільки Rational, int або str")
+        
+    def __call__(self, index): # Метод для отримання значення за індексом
+         return self.rationals[index]
     
     
     def __len__(self):
      return len(self.rationals)
-
     
 
+    def __iadd__(self, other):
+        if isinstance(other, RationalList):
+            self.rationals += other.rationals
+        else:
+            self.rationals.append(Rational(other,1))
+        return self
+    
+
+
+
+
+
 f = RationalList(["4/3", "2/5", "1/2"])
-print(f)
-print(len(f))
+print(f"кількість елементів у списку: {len(f)}")
+############################################################## 
+
+
+# iadd
+f += "3/7"
+  # 4/3, 2/5, 1/2, 3/7
+print(f"кількість елементів у списку: {len(f)}")
+
+g = RationalList(["1/1"])
+f += g
+ # 4/3, 2/5, 1/2, 3/7, 1/1
+print(f"кількість елементів у списку: {len(f)}")
+
+
+f += 10
+ # 4/3, 2/5, 1/2, 3/7, 1/1
+print(f"кількість елементів у списку: {len(f)}")
+
+
+############################################################## 
+r1 = RationalList(["1/2", "2/3"])
+r2 = RationalList(["4/5"])
+
+r3 = r1 + r2         # Два списки
+r4 = r1 + 3          # Додати int
+r5 = r1 + Rational(5, 2)  # Додати Rational
+
+print(r1[1])
+
+print(r3[2])
+
+
+
+def Distribution(file_name):
+    d=open(file_name) 
+    
+    for l in d:
+            print(l)
+            l = l.split()
